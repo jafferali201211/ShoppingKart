a353d73889119d95303d2ce1fba3dadb4c5c9ee4
jafferAli <jafferali201211@gmail.com>
2019-12-30 23:20:40 +0530

    Event Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsComponent } from './products/products.component';
+import { ClothingComponent } from './products/clothing/clothing.component';
+import { ElectronicsComponent } from './products/electronics/electronics.component';
+import { FurnitureComponent } from './products/furniture/furniture.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    ProductsComponent,
+    ClothingComponent,
+    ElectronicsComponent,
+    FurnitureComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/utility/userNamePipe.ts
@@ -0,0 +1,7 @@
+// import {Pipe , PipeTransform} from '@angular/core';
+
+// @Pipe({name : 'formatUserName'})
+
+// export class FormatUserNamePipe implements PipeTransform{
+//     TransForm()
+// }
